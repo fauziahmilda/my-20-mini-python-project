38e3f48ca31a7fc613e94284a00df78da6bfcad6
fauziahmilda <fauziah.milda@gmail.com>
2023-01-21 19:44:36 +0700

    add calculator file

--- p2_word_replacement/app.py
+++ p2_word_replacement/app.py
@@ -1,1 +1,14 @@
 # replace method in python
+
+def replace_word():
+    str = input("Enter random quote: ")
+    print("-------------------------------")
+    print(f"This is your quote: {str}")
+
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    print("After replacement: ", str.replace(
+        word_to_replace, word_replacement))
+
+
+replace_word()
---
+++ p3_basic_calculator/calculator_app.py
@@ -0,0 +1,3 @@
+# define the function needed: add, sub, mul, and div
+# call the function
+# print option to the user
