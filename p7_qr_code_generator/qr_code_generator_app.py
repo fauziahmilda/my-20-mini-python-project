3e9f7a719f6f985c2e3a7fed6ab89574aa1ef4c2
fauziahmilda <fauziah.milda@gmail.com>
2023-01-28 21:51:48 +0700

    import lib

---
+++ p7_qr_code_generator/qr_code_generator_app.py
@@ -0,0 +1,7 @@
+# install all the liblaries needed
+# pip install -r requirements.txt
+# create the function that collect a text and convert it to a qrcode
+# save qr code as an image
+# run the function
+
+import qrcode
