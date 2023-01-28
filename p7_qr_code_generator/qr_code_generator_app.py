aa5bf890fdc1e29d738d490df0c3a49cdb09d7e9
fauziahmilda <fauziah.milda@gmail.com>
2023-01-28 22:05:48 +0700

    qrcode generator done

--- p7_qr_code_generator/qr_code_generator_app.py
+++ p7_qr_code_generator/qr_code_generator_app.py
@@ -5,3 +5,23 @@
 # run the function
 
 import qrcode
+
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.ERROR_CORRECT_L,
+        box_size=10,
+        border=4
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    name_img = input("Save nama file: ")
+    image_path = "p7_qr_code_generator/qrcode"
+    img.save(f"{image_path}/{name_img}.png")
+
+
+codeUser = input("Input link: ")
+generate_qrcode(codeUser)
