3d9128844e6bd2724279929fd6e2add569c9362f
fauziahmilda <fauziah.milda@gmail.com>
2023-01-28 18:31:08 +0700

    add dictionary list

---
+++ p6_quiz_program/quiz_app.py
@@ -0,0 +1,33 @@
+# a dictionary that store questions and answers
+# have a variable that tracks the score of the player
+# loop  through the dictionary using the key values pairs
+# display each questions to the user and allow them to answer
+# tell them if they are right or wrong
+# show the final result when quiz is complete
+
+quiz = {
+    "question1": {
+        "question": "what is the formula for mechanical energy",
+        "answer": "EM = EK + EP"
+    },
+    "question2": {
+        "question": "What is formula of gravity?",
+        "answer": "F = G x (m1 x m2)/r2"
+    },
+    "question3": {
+        "question": "What is the unit of force?",
+        "answer": "Newton"
+    },
+    "question4": {
+        "question": "Who find gravitaion laws?",
+        "answer": "Isaac Newton"
+    },
+    "question5": {
+        "question": "Who find relativity laws?",
+        "answer": "Albert Einstain"
+    },
+    "question6": {
+        "question": "Who find Coservation Laws of Energy?",
+        "answer": "James Prescott Joule"
+    },
+}
