1d6c41d6edf5bdb8e35ae540351d6fcd20d65122
fauziahmilda <fauziah.milda@gmail.com>
2023-01-30 18:38:21 +0700

    dice rolling done

--- p10_dice_roll_simulator/dice_rolling_simulator_app.py
+++ p10_dice_roll_simulator/dice_rolling_simulator_app.py
@@ -1,8 +1,65 @@
+import random
 import os
 
+# create the dictionary
+dice_drawing = {
+    1: (
+        ".___________.",
+        "|1          |",
+        "|           |",
+        "|     ♥️     |",
+        "|           |",
+        "|___________|",
+    ),
+    2: (
+        ".___________.",
+        "|2          |",
+        "|   ♥️       |",
+        "|           |",
+        "|       ♥️   |",
+        "|___________|",
+    ),
+    3: (
+        ".___________.",
+        "|3          |",
+        "|   ♥️       |",
+        "|     ♥️     |",
+        "|       ♥️   |",
+        "|___________|",
+    ),
+    4: (
+        ".___________.",
+        "|4          |",
+        "|   ♥️   ♥️   |",
+        "|           |",
+        "|   ♥️   ♥️   |",
+        "|___________|",
+    ),
+    5: (
+        ".___________.",
+        "|5          |",
+        "|   ♥️   ♥️   |",
+        "|     ♥️     |",
+        "|   ♥️   ♥️   |",
+        "|___________|",
+    ),
+    6: (
+        ".___________.",
+        "|6          |",
+        "|   ♥️ ♥️ ♥️   |",
+        "|           |",
+        "|   ♥️ ♥️ ♥️   |",
+        "|___________|",
+    ),
 
+
+
+}
+
+
+# main function
 def main():
-    option = input("Roll again? (y/n) ")
+    option = input("Roll the dice? (y/n) ")
     if option.lower() == "y":
         os.system('cls')
         roll_dice()
@@ -16,5 +73,14 @@ def main():
         main()
 
 
+# funtion to roll the dice
 def roll_dice():
-    pass
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    print(f"dice rolled : {dice1} and {dice2}")
+    print("\n".join(dice_drawing[dice1]))
+    print("\n".join(dice_drawing[dice2]))
+    main()
+
+
+main()
