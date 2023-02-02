17073402cb2c8fc765ade8dbc108258c5e1c8917
fauziahmilda <fauziah.milda@gmail.com>
2023-02-03 00:14:20 +0700

    word dictionary done

---
+++ p19_dictionary/word_dictionary_v2_app.py
@@ -0,0 +1,8 @@
+from PyDictionary import PyDictionary
+
+
+word = input("Enter the word: ")
+dictionary = PyDictionary(word)
+print(dictionary.getMeanings())
+
+# this program just using library PyDictionary
