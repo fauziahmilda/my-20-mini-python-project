1d0bfb6ff1fabe26b61214c875a73a8ccfaad640
fauziahmilda <fauziah.milda@gmail.com>
2023-02-05 17:38:10 +0700

    face detector done

--- p15_face_detection/face_detection_app.py
+++ p15_face_detection/face_detection_app.py
@@ -1,2 +1,17 @@
 import cv2
 
+face_cascade = cv2.CascadeClassifier('p15_face_detection\haarcascade_frontalface_default.xml')
+
+img = cv2.imread('p15_face_detection/test.png')
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x, y, w, h) in faces:
+    cv2. rectangle(img, (x, y), ( x+ w, y + h), (225, 0, 0), 2)
+
+cv2.imshow('img', img)
+cv2.waitKey()
+
+cv2.imwwrite("face_detected.png", img)
